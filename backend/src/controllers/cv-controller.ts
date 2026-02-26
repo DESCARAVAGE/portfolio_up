13de2fd4b55441d01e0e2b83f05b48ca8cae66fc
DESCARAVAGE <escaravage.daniel@gmail.com>
2026-02-26 10:47:44 +0100

    Add Docker cv_pdf service

--- backend/src/controllers/cv-controller.ts
+++ backend/src/controllers/cv-controller.ts
@@ -5,8 +5,9 @@ export function downloadCv(_req: Request, res: Response) {
     try {
         const stream = getCvStream();
 
+        // en HTTP, un téléchargement repose sur les Headers de réponse, lien avec le type 
         res.setHeader("Content-Type", "application/pdf");
-        res.setHeader("Content-Dispositon", 'attachment; filename="exemple.pdf');
+        res.setHeader("Content-Disposition", 'attachment; filename="exemple.pdf');
 
         stream.pipe(res);
     } catch (error) {
---
+++ cv_pdf/src/index.ts
@@ -0,0 +1,18 @@
+import express, { Response, Request } from "express";
+import cors from "cors";
+import multer from "multer";
+import path from "path";
+import fs from "fs";
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+
+app.get("/cv_pdf", (_: Request, res: Response) => {
+    res.send("Hello World !");
+});
+
+app.listen(port, () => {
+            console.log(`[server]: Server is running at http://localhost:${port}/cv_pdf`);
+        });
