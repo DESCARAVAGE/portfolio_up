0c4b79c1bbe1f7e81850e0c2f9fadf2f887635cd
DESCARAVAGE <escaravage.daniel@gmail.com>
2026-03-30 15:09:38 +0200

    Fix the dev env

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -26,7 +26,7 @@ export function Navbar(): React.JSX.Element {
         endIcon={<FileDownloadIcon />}
         onClick={handleDownloadCv}
       >
-        CVCV
+        CV
       </Button>
     </Box>
   );
